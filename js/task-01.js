6f10d5d3ea018d827ed157ecf29b173e1db71718
Seliutin-Roman-UA <selutin@gmail.com>
2022-09-14 21:06:24 +0300

    HW 06 v1.1

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
-let categories = document.querySelector("#categories");
+
 let childrenOfcategories = document.querySelector("#categories").children;
-console.log(childrenOfcategories);
+console.log("Number of categories: ", childrenOfcategories.length);
 for (const node of childrenOfcategories) {
     console.log("Categories: ", node.children[0].textContent);
     console.log("Elements: ", node.children[1].children.length);
